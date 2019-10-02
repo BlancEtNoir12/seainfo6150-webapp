b62cc1da23ed9ba50068ba1d6201f1fe226c708b
BlancEtNoir12 <BlancEtNoir12@users.noreply.github.com>
2019-10-01 23:32:54 -0700

    upload assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Button from './ArticleListItem';
 
 const ArticleList = props => {
   return (
     <ul>
       {props.articles.map(article => (
-        <li key={article.slug}>ArticleListItem component goes here</li>
+          <li key={article.slug}>
+              <h3>{article.title}</h3>
+              <p>{article.shortText}</p>
+              <p>{article.pubDate}
+                  <Button info={article} />
+              </p>
+          </li>
       ))}
     </ul>
   );
