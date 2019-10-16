394262967dba75571d8cb5fe8de67f2610b87d59
BlancEtNoir12 <BlancEtNoir12@users.noreply.github.com>
2019-10-16 00:40:34 -0700

    Assignment6

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SlugButton from './SlugButton';
+import ArticleImage from './ArticleImage';
 import styles from './ArticleListItem.module.css';
 
 const Item = props => {
     return (
         <div className={styles.body} >
-            <h1 className={styles.header3}> { props.article.title }</h1>
-            <p>{props.article.shortText}</p>
-            <time className={styles.time} dateTime={props.article.pubDate}>
-                {props.article.pubDate}
+            <ArticleImage info={props.article}></ArticleImage>
+            <div style={{ border: '1em' }}>
+                <h1 className={styles.header3}> {props.article.title}</h1>
+                <p>{props.article.shortText}</p>
+                <time className={styles.time} dateTime={props.article.pubDate}>
+                    {props.article.pubDate}
+                </time>
                 <SlugButton info={props.article}>
                     show article slug
-            </SlugButton>
-            </time>
+                </SlugButton>
+            </div>
         </div>
     );
 };
